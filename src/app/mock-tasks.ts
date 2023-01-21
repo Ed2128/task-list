7d7317c83edab0bd4d22a4dce421590b94a53f5c
Ed2128 <edujnavarro7@gmail.com>
2023-01-21 09:20:33 -0300

    tasklist completa

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASKS:Task[]= [
+    {
+        id: 1,
+        text: "Terminar primer App en Angular",
+        day: 'Marzo 22 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Descansar",
+        day: 'Marzo 23 a las 12:00',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: "Empezar modulo 4",
+        day: 'Marzo 24 a las 10:00',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Preparse pa la facu",
+        day: 'Marzo 27 a las 18:00',
+        reminder: true
+    }
+]
